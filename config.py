12a60098726dc0a8ca2d97a03b63822d0dcf37a6
Dor-K <134565994+Dor-K@users.noreply.github.com>
2023-06-22 19:41:56 -0400

    Adding config.py file
    Config.py file to store API keys securely. Already added to .gitignore

---
+++ config.py
@@ -0,0 +1,11 @@
+#File to store API keys for RealBuddy
+
+
+#RealtyMole API Key - Dor
+
+rm_key = 'test1'
+
+#RealtyMole Host 
+#At the moment I'm not sure if we need this in this file but I'm putting it here just in case 
+
+rm_host = 'test2'
